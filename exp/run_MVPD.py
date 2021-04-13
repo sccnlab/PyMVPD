710a9506cb7912ee45d5db422f02217a89218ffb
Mengting Fang <fangmg@sirius.cm.cluster>
2021-04-13 12:55:25 -0400

    update

--- exp/run_MVPD.py
+++ exp/run_MVPD.py
@@ -66,4 +66,7 @@ data_loading.load_data(sub, total_run, roi_1_name, roi_2_name, filepath_func, fi
 """
 Step 3 - Analysis Execution
 """
-model_exec.MVPD_exec(model_type, sub, total_run, alpha, num_pc, roidata_save_dir, roi_1_name, roi_2_name, filepath_func, filepath_mask1, filepath_mask2, results_save_dir, save_prediction)
+model_exec.MVPD_exec(model_type, sub, total_run, 
+                     alpha, num_pc, # reg params
+                     input_size, output_size, hidden_size, num_epochs, save_freq, print_freq, batch_size, learning_rate, momentum_factor, w_decay, # nn params 
+                     roidata_save_dir, roi_1_name, roi_2_name, filepath_func, filepath_mask1, filepath_mask2, results_save_dir, save_prediction)
